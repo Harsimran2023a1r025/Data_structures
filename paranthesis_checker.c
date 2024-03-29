21f6a5c786bb8985b0ff9a9287d802618b4eaff9
Harsimran2023a1r025 <146611938+Harsimran2023a1r025@users.noreply.github.com>
2024-03-29 14:34:12 +0530

    Update paranthesis_checker.c

--- paranthesis_checker.c
+++ paranthesis_checker.c
@@ -1,82 +1,112 @@
 #include <stdio.h>
-#include<string.h>
-#define max 10
+#include <string.h>
+#include <ctype.h>
+#define max 100
 char stack[max];
-int i,top=-1;
-void push(char);
-char pop();
-
-void main()
+int top = -1;
+void push(char stack[], char);
+char pop(char stack[]);
+void infixtopostfix(char source[], char target[]); // source is given expression and target is the postfix
+int getpriority(char);                             // to check the presedence of operators
+int main()
 {
-    char expression[max], temp;
-    int count = 1;
-    
-    printf("Enter the expression\n");
-    scanf("%s", expression);
-    int a=strlen(expression);
-    for ( i = 0; i < a; i++)
+    char infix[100], postfix[100];
+    printf("Enter the infix expression\n");
+    scanf("%s", &infix);
+    strcpy(postfix, "");            // to make postfix string empty all 100 positions
+    infixtopostfix(infix, postfix); // calling the function
+    printf("\nthe postfix expression is\n");
+    printf("%s", postfix);
+}
+void infixtopostfix(char source[], char target[])
+{
+    int i = 0, j = 0; // i to iterate through each character of given string or source and j is to iterate through every character of target string
+    char temp;
+    strcpy(target, "");       // to make target string empty
+    while (source[i] != '\0') // runs till the iteration found the empty index position
     {
-        if (expression[i] == '(' || expression[i] == '{' || expression[i] == '[' )
+        if (source[i] == '(')
         {
-            push(expression[i]);
+            push(stack, source[i]); // acccording to algorithm when we find open brace push it into stack
+            i++;
         }
-        else if(expression[i] == ')' || expression[i] == '}' || expression[i] == ']' )
-        {
-            if(top==-1)
-            {
-                count=0;
-            }
-            else{
-                temp=pop();
-                if(expression[i] == ')' && (expression[i] == '}' || expression[i] == ']'))
-                {
-                    count=0;
-                }
-                else if(expression[i] == '}' && (expression[i] == ')' || expression[i] == ']'))
-                {
-                    count=0;
-                }
-                else if (expression[i] == ']' &&( expression[i] == ')' || expression[i] == '}'))
-                {
-                    count=0;
-                }
 
-            }
+        else if (isdigit(source[i]) || isalpha(source[i]))
+        {
+            target[j] = source[i]; // according to algorithm if we fing digit or alpha add it to the postfix target is the postfix here
+            j++;
+            i++;
         }
-        
-    }
-    if(top>=0)
-    {
-        count=0;
-    }
-    if(count==1)
-    {
-        printf("Valid expression\n");
-    }
-    else{
-        printf("Not valid expression\n");
+        else if (source[i] == '+' || source[i] == '-' || source[i] == '*' || source[i] == '/' || source[i] == '%') // to check the operators
+        {
+            while ((top != -1) && (stack[top] != '(') && (getpriority(stack[top]) > getpriority(source[i]))) // to check the presedence of operators
+            {         //if any of the condition is false it push that character to stack else it will pop                                                                                       // if stack operator has greater presednec than reading pop it from stack and add to the postfix expression
+                target[j] = pop(stack);
+                j++;
+            }
+            push(stack, source[i]);//to push the operators 
+            i++;
+        }
+        else if (source[i] == ')')
+        {
+            while ((top != -1) && (stack[top] == '(')) // runs until top become -1 or until it get the open brace as per the algorithm
+            {
+                target[j] = pop(stack); // the element which delete stores in target string
+                j++;
+            }
+            if (top == -1)
+            {
+                printf("Invalid expression\n"); // if the entered string is empty
+               
+            }
+            temp = pop(stack); // to remove the open brace like if expression is -(a+b)
+            i++;
+        }
+         else
+        {
+            printf("\nInvalid expression");
+            
+        }
+       
+        while ((top != -1) && (stack[top] != '('))//if their are any other elements inn stack not followed by the uper conditions then it follows that
+        {
+            target[j] =pop(stack);
+            j++;
+        }
+        target[j] = '\0';
     }
 }
-void push(char c)
+int getpriority(char o)//to check the presedence of operators
 {
-    
+    if ((o=='/')||(o=='*')||(o=='%'))
+    {
+        return 1;
+    }
+    else if((o=='+')||(o=='-'))
+    {
+        return 0;
+    }
+}
+void push(char stack[],char val)//to push the elements
+{
     if(top==max-1)
     {
-        printf("\n overflow");
+        printf("It is overflow");
     }
     else{
-        top=top+1;
-        stack[top]=c;
+        top++;
+        stack[top]=val;
     }
-    printf("\n");
 }
-char pop(){
+char pop(char stack[])//to delete the elemets
+{
+    char val=' ';//assigning character value as empty so it can hold stack characters
     if(top==-1)
     {
-        printf("Underflow");
+        printf("UNder flow");
     }
     else{
-        return (stack[top--]);
+        val=stack[top];
+        top--;
     }
 }
- 
